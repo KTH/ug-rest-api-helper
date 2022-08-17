102a0dd89095dd66a25ca757c86cbdb39c9a58e7
Rao-Sabahat-Ali <raosabahat01@live.com>
2022-08-17 15:51:03 +0200

    fix: remove window to verify token

--- tokenCall.js
+++ tokenCall.js
@@ -13,15 +13,10 @@ let token = null
  * @param {*} token Token to verify if expired or not
  * @returns Will return boolean. True is for expired and False is for not expired
  */
-const isTokenExpired = (token) => {
+ const isTokenExpired = (token) => {
   let tokenExpired = true
   if (token) {
-    var base64Url = token.split('.')[1]
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-    }).join(''))
-    const tokenPayload = JSON.parse(jsonPayload)
+    const tokenPayload = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
     const expiryTime = tokenPayload.exp
     if (Date.now() < expiryTime * 1000) {
       tokenExpired = false
